5aa5f36b2efc3f4a41cdc3c60045619c77a4e699
Tonia Tkachuk <tonia.tkachuk@gmail.com>
2018-12-20 10:41:54 +0100

    Repo details layout

--- app/src/main/java/com/tonia/githubandroidtrending/network/GitHubApi.kt
+++ app/src/main/java/com/tonia/githubandroidtrending/network/GitHubApi.kt
@@ -5,13 +5,14 @@ import com.tonia.githubandroidtrending.model.TrendingAndroidReposResponse
 import io.reactivex.Flowable
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 /**
  * Interface, which contains the list of GitHub API calls.
  */
 interface GitHubApi {
-    @GET("/repos/")
-    fun getTrendingAndroidRepos(): Flowable<TrendingAndroidReposResponse>
+    @GET("search/repositories?q=android&sort=stars&order=desc&per_page=10")
+    fun getTrendingAndroidRepos(@Query("page") pageToFetch: Int): Flowable<TrendingAndroidReposResponse>
 
     @GET("/repos/{owner}/{repo}")
     fun getRepoDetails(@Path("owner") owner: String, @Path("repo") repo: String): Flowable<RepoDetailsResponse>
