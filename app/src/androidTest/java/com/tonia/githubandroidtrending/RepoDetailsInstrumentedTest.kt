f4eb39c2a9b34e0094bb16bfd7e97ff90e855103
Tonia Tkachuk <tonia.tkachuk@gmail.com>
2019-01-08 21:15:39 +0100

    Added text labels test for Repo Details fragment.

--- app/src/androidTest/java/com/tonia/githubandroidtrending/RepoDetailsInstrumentedTest.kt
+++ app/src/androidTest/java/com/tonia/githubandroidtrending/RepoDetailsInstrumentedTest.kt
@@ -11,11 +11,13 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import com.tonia.githubandroidtrending.model.License
 import com.tonia.githubandroidtrending.model.Owner
 import com.tonia.githubandroidtrending.model.Repo
+import com.tonia.githubandroidtrending.util.getRepoDate
 import junit.framework.Assert.assertEquals
 import org.hamcrest.Matchers.allOf
 import org.junit.Before
@@ -24,41 +26,56 @@ import org.junit.Test
 
 class RepoDetailsInstrumentedTest {
 
+    private val repo = Repo(
+        id = "1",
+        name = "octorepo",
+        full_name = "github/octorepo",
+        html_url = "https://github.com/googlesamples",
+        description = "A collection of samples to discuss and showcase different architectural tools and patterns for Android apps.",
+        language = "Kotlin",
+        stargazers_count = 23,
+        forks_count = 9,
+        watchers_count = 48,
+        open_issues_count = 14,
+        updated_at = "2018-12-25T18:50:51Z",
+        owner = Owner(
+            login = "github",
+            id = 53,
+            avatar_url = "https://avatars3.githubusercontent.com/u/7378196?v=4",
+            url = "https://api.github.com/users/googlesamples"
+        ),
+        license = License(
+            key = "apache-2.0",
+            name = "Apache License 2.0",
+            url = "https://api.github.com/licenses/apache-2.0"
+        )
+    )
+
     @get:Rule
     val intentTestRule = IntentsTestRule(MainActivity::class.java)
 
     @Before
     fun showRepoDetailsFragment() {
-        intentTestRule.activity.showRepoDetails(Repo(
-            id = "1",
-            name = "octorepo",
-            full_name = "github/octorepo",
-            html_url = "https://github.com/googlesamples",
-            description = "A collection of samples to discuss and showcase different architectural tools and patterns for Android apps.",
-            language = "Kotlin",
-            stargazers_count = 23,
-            forks_count = 9,
-            watchers_count = 48,
-            open_issues_count = 14,
-            updated_at = "2018-12-25T18:50:51Z",
-            owner = Owner(
-                login = "github",
-                id = 53,
-                avatar_url = "https://avatars3.githubusercontent.com/u/7378196?v=4",
-                url = "https://api.github.com/users/googlesamples"
-            ),
-            license = License(
-                key = "apache-2.0",
-                name = "Apache License 2.0",
-                url = "https://api.github.com/licenses/apache-2.0"
-            )
-        ))
+        intentTestRule.activity.showRepoDetails(repo)
     }
 
     @Test
     fun useAppContext() {
         val appContext = ApplicationProvider.getApplicationContext<Context>()
         assertEquals("com.tonia.githubandroidtrending", appContext.packageName)
+    }
+
+    @Test
+    fun repoDetails_textLabelsCheck() {
+        onView(withId(R.id.textViewStars)).check(matches(withText(repo.stargazers_count.toString())))
+        onView(withId(R.id.textViewForks)).check(matches(withText(repo.forks_count.toString())))
+        onView(withId(R.id.textViewWatchers)).check(matches(withText(repo.watchers_count.toString())))
+        onView(withId(R.id.textViewOpenIssues)).check(matches(withText(repo.open_issues_count.toString())))
+        onView(withId(R.id.textViewLanguage)).check(matches(withText(repo.language)))
+        onView(withId(R.id.textViewLicense)).check(matches(withText(repo.license?.name)))
+        onView(withId(R.id.textViewLastUpdated)).check(matches(withText(getRepoDate(repo.updated_at))))
+        onView(withId(R.id.textViewFullName)).check(matches(withText(repo.full_name)))
+        onView(withId(R.id.textViewDesc)).check(matches(withText(repo.description)))
     }
 
     @Test
