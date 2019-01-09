5db284805ed42822a7201f339984ada198000c3a
Tonia Tkachuk <tonia.tkachuk@gmail.com>
2019-01-09 12:26:16 +0100

    Search url fix.

--- app/src/main/java/com/tonia/githubandroidtrending/network/GitHubApi.kt
+++ app/src/main/java/com/tonia/githubandroidtrending/network/GitHubApi.kt
@@ -11,7 +11,7 @@ import retrofit2.http.Query
  * Interface, which contains the list of GitHub API calls.
  */
 interface GitHubApi {
-    @GET("search/repositories?q=android&sort=stars&order=desc&per_page=10")
+    @GET("/search/repositories?q=android&sort=stars&order=desc&per_page=10")
     fun getTrendingAndroidRepos(@Query("page") pageToFetch: Int): Single<TrendingAndroidReposResponse>
 
     @GET("/repos/{owner}/{repo}/readme")
